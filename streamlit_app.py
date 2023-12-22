3b16476a047d4d56e90631922613efedc2b8bc4c
musamike <151539798+musamike@users.noreply.github.com>
2023-12-22 16:28:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-pip install snowflake-connector-python
-pip install pycryptodome
 
