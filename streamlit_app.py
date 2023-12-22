b731a311eeaf782641e7adbd39cbc6af91db400f
musamike <151539798+musamike@users.noreply.github.com>
2023-12-22 21:45:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,45 +26,41 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import streamlit as st
-import pandas as pd
 import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'Kiwi')
 
-# Assuming my_fruit_list is a pandas DataFrame
-my_fruit_list = pd.DataFrame({'Fruit': ['Apple', 'Banana', 'Orange']})
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-st.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
-# New Section to display fruityvice api response
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered ', fruit_choice)
+#New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-# Make the API request and check for success
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-if fruityvice_response.status_code == 200:
-    # Import the pandas module and normalize the JSON response
-    import pandas as pd
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    # Display the normalized data
-    st.dataframe(fruityvice_normalized)
-else:
-    st.write(f"Fruityvice API request failed with status code: {fruityvice_response.status_code}")
 
-# Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = st.multiselect("Pick some fruits:", my_fruit_list['Fruit'].tolist(), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list[my_fruit_list['Fruit'].isin(fruits_selected)]
-
-# Display the table on the page.
-st.dataframe(fruits_to_show)
-
-# New section to display fruityvice api response
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered ', fruit_choice)
-
-# import Snowflake connector
+#import connector
 import snowflake.connector
+
+
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur_rows = my_cnx.fetchall()
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
 
 
