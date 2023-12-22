a54c9d222f6d5a0b26358760f533b9ccdf7aa10d
musamike <151539798+musamike@users.noreply.github.com>
2023-12-22 19:07:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,28 +10,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-try:
-    # Streamlit < 0.65
-    from streamlit.ReportThread import get_report_ctx
 
-except ModuleNotFoundError:
-    try:
-        # Streamlit > 0.65
-        from streamlit.report_thread import get_report_ctx
-
-    except ModuleNotFoundError:
-        try:
-            # Streamlit > ~1.3
-            from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
-
-        except ModuleNotFoundError:
-            try:
-                # Streamlit > ~1.8
-                from streamlit.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
-
-            except ModuleNotFoundError:
-                # Streamlit > ~1.12
-                from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
